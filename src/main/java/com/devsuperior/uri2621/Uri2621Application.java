0452e3c3edb6749ea31465f29806ee1e4916a46e
micael1ma <micael1ma.professional@gmail.com>
2025-02-20 12:08:22 -0300

    SQL raiz

--- src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2621;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2621.repositories.ProductRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2621Application implements CommandLineRunner {
@@ -19,6 +24,12 @@ public class Uri2621Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		List<ProductMinProjection> list = repository.serach1();
+		List<ProductMinDTO> result1 = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
+
+		for (ProductMinDTO obj : result1) {
+			System.out.println(obj);
+		}
 	}
 }
